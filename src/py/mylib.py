6456dc0b7c84416f669b0f6e69731f156213bfe9
Jun Kai Liao <2003junkai@gmail.com>
2022-02-23 20:28:17 -0500

    3 channel 1024x756 CNN update

--- src/py/mylib.py
+++ src/py/mylib.py
@@ -16,7 +16,7 @@ def importTarget(filepath, scale = 0.25, side = 32):
     for i in range(len(shapes)):
         x_pos = round(shapes[i].get("points")[0][1]*scale)
         y_pos = round(shapes[i].get("points")[0][0]*scale)
-        target[x_pos][y_pos] = 1
+        target[x_pos][y_pos] += 1
     target = F.pad(target, pad=(16, 16 ,16 ,16), value=0)
     target_map = torch.empty(1008, 756)
     for i in tqdm(range(1008-side)):
@@ -29,23 +29,13 @@ def cellCount(map):
     return torch.sum(map)/(1024)
 
 def importPicture(path):
-    img = Image.open(path).convert("L").resize((756, 1008))
+    img = Image.open(path).resize((756, 1008))
     img = transforms.ToTensor()(img)
     return F.pad(img, pad=(16, 16 ,16 ,16), value=0)
 
 def partition(image, size = 32):
-    t_images = torch.empty(1008*756, 32, 32)
-    for i in tqdm(range(image.shape[1]-size)):
-        for k in (range(image.shape[2]-size)):
+    t_images = torch.empty(1008*756, 3, 32, 32)
+    for i in tqdm(range(image.shape[-2]-size)):
+        for k in (range(image.shape[-1]-size)):
             t_images[k + 756 * i] = transforms.functional.crop(image, i, k, size, size)
     return t_images
-
-'''
-def partition(image, size = 32, device = "cuda"):
-    t_images = torch.empty((0, size, size)).to(device)
-    for i in tqdm(range(image.shape[1]-size)):
-        for k in (range(image.shape[2]-size)):
-            t_images = torch.cat((t_images, transforms.functional.crop(image, k, i, size, size)), axis = 0)
-
-    return t_images
-'''
